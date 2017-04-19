076ad1dcf56a2d3b56ee3f54803402586335730b
Jason Duffy <jasonsduffy@gmail.com>
2017-04-19 14:28:49 -0400

    add some comments to scripts; use .csproj instead of .dll's

--- ci/setenv.sh
+++ ci/setenv.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# solves issue with windows backslashes in .csproj files when using nunit-console
 export MONO_IOMAP=all
 
+# solves error about xamarin adb version mismatch
 rm ~/Library/Developer/Xamarin/android-sdk-macosx/platform-tools/adb
 ln -s $(which adb) ~/Library/Developer/Xamarin/android-sdk-macosx/platform-tools/adb
--- ci/uiTest.sh
+++ ci/uiTest.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# build apk for running ui tests
 xbuild /t:SignAndroidPackage /p:Configuration=Release Droid/SampleApp.Droid.csproj
+
 xbuild SampleApp.UITests/SampleApp.UITests.csproj
-mono ./NUnit.Runners.2.6.4/tools/nunit-console.exe -xml:test-results/UiTestResult.xml SampleApp.UITests/bin/Debug/SampleApp.UITests.dll
+
+# need to use mono to run specific version of nunit-console
+mono ./NUnit.Runners.2.6.4/tools/nunit-console.exe -xml:test-results/UiTestResult.xml SampleApp.UITests/SampleApp.UITests.csproj
