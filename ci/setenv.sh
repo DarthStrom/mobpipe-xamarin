ec6e389bff62df267ab49749ce4660f50725d4d3
XPO Dev <xpodev@XPOs-MacBook-Pro.local>
2017-04-21 15:15:01 -0400

    add nuget restore to setenv

--- ci/setenv.sh
+++ ci/setenv.sh
@@ -3,6 +3,4 @@
 # solves issue with windows backslashes in .csproj files when using nunit-console
 export MONO_IOMAP=all
 
-# solves error about xamarin adb version mismatch
-rm ~/Library/Developer/Xamarin/android-sdk-macosx/platform-tools/adb
-ln -s $(which adb) ~/Library/Developer/Xamarin/android-sdk-macosx/platform-tools/adb
+nuget restore
